56841a6e3a6f8d89866e20d0b56c042ce01d659e
Nedim Karčić <nedimkarcic52@gmail.com>
2019-12-02 13:23:23 +0100

    The game is playable now

---
+++ app.js
@@ -0,0 +1,47 @@
+const userInput = document.querySelector('#userInput');
+const randomNumber = Math.floor(Math.random() * 101); 
+const button = document.querySelector('#button');
+const message = document.querySelector('#message');
+let guessCounter = 1;
+
+
+
+function equalCheck(e) {
+	if (guessCounter === 1 && userInput === randomNumber) {
+		message.textContent = 'Congratulations!';
+	};
+
+	if (guessCounter === 1 && userInput !== randomNumber) {
+		message.textContent = 'Not correct! You have 2 guesses left!';
+	}
+
+	if (guessCounter === 2 && userInput === randomNumber) {
+		message.textContent = 'Congratulations!';
+	};
+
+	if (guessCounter === 2 && userInput !== randomNumber) {
+		message.textContent = 'Not correct! You have 1 guess left!';
+	}
+
+	if (guessCounter === 3 && userInput === randomNumber) {
+		message.textContent = 'Congratulations!';
+	};
+
+	if (guessCounter === 3 && userInput !== randomNumber) {
+		message.textContent = 'Game Over!';
+	}
+
+	userInput.value = '';
+	guessCounter++;
+
+};
+
+function equalCheckOnEnter(e){
+	if(e.keyCode === 13){
+	equalCheck();
+};
+};
+
+
+button.addEventListener('click', equalCheck);
+userInput.addEventListener('keyup', equalCheckOnEnter);
